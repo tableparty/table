d1dc8a7b69af36b6c74228fcbb1502a8148afa51
John Bannister <jdbann@gmail.com>
2020-03-19 15:17:24 +0000

    Default primary keys to uuids

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module Table
     # here. Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |generators|
+      # Default primary keys to uuids
+      generators.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_19_151524) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
 end
