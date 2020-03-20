45cf16849c7c5c82324c55db77e00db95d4831f6
John Bannister <jdbann@gmail.com>
2020-03-20 10:56:05 +0000

    Refactor to bind channel functions to controller

--- app/javascript/controllers/campaign_controller.js
+++ app/javascript/controllers/campaign_controller.js
@@ -5,26 +5,27 @@ export default class extends Controller {
   static targets = ["mapName", "mapImage", "map"]
 
   connect() {
-    const campaignId = this.element.dataset.id
-    const maps = this.mapTargets
-    const mapName = this.mapNameTarget
-    const mapImage = this.mapImageTarget
-    consumer.subscriptions.create({
+    this.campaignId = this.element.dataset.id
+    this.channel = consumer.subscriptions.create({
       channel: "CampaignChannel",
-      id: campaignId
+      id: this.campaignId
     }, {
-      received(data) {
-        mapName.innerHTML = data.current_map.name
-        mapImage.src = data.current_map_image
-      },
-      connected() {
-        const changeMap = map => {
-          this.perform("change_current_map", { map_id: map.dataset.mapId, campaign_id: campaignId })
-        }
-        maps.forEach(map => {
-          map.addEventListener("click", () => changeMap(map))
-        })
-      }
+      received: this.cableReceived.bind(this),
+      connected: this.cableConnected.bind(this)
+    })
+  }
+
+  cableReceived(data) {
+    this.mapNameTarget.innerHTML = data.current_map.name
+    this.mapImageTarget.src = data.current_map_image
+  }
+
+  cableConnected() {
+    const changeMap = map => {
+      this.channel.perform("change_current_map", { map_id: map.dataset.mapId, campaign_id: this.campaignId })
+    }
+    this.mapTargets.forEach(map => {
+      map.addEventListener("click", () => changeMap(map))
     })
   }
 }
