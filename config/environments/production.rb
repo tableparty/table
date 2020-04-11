686fa3a21cc8b980b62bd3d3c9741eba726e4705
Chad Pytel <chad@thoughtbot.com>
2020-04-11 15:13:21 -0400

    Use APP_HOST instead of HOST
    HOST conflicts with an internal rails env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: ENV["SENDGRID_USERNAME"],
     password: ENV["SENDGRID_PASSWORD"],
-    domain: ENV["HOST"],
+    domain: ENV["APP_HOST"],
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,5 +1,5 @@
 Rails.application.config.action_mailer.default_url_options = {
-  host: ENV["HOST"]
+  host: ENV["APP_HOST"]
 }
 Rails.application.routes.default_url_options =
   Rails.application.config.action_mailer.default_url_options
