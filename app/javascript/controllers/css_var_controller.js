19878de38cdde80fcb425359a3b3a4d016950422
John Bannister <jdbann@gmail.com>
2020-04-11 09:07:43 +0100

    Remove console log

--- app/javascript/controllers/css_var_controller.js
+++ app/javascript/controllers/css_var_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log("connected")
     this.observer = new MutationObserver(mutations => {
       mutations.forEach(({ target, attributeName }) => {
         target.dataset.cssVars.split(" ").forEach(cssVar => {
