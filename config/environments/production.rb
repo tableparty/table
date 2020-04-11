f387e8c1567e94f248df018c2232ac2f4c8b3b3d
Chad Pytel <chad@thoughtbot.com>
2020-04-11 14:55:39 -0400

    Configure with Sendgrid for email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,13 +119,16 @@ Rails.application.configure do
   #   ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context =
   #   ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  ActionMailer::Base.smtp_settings = {
-    port: ENV["MAILGUN_SMTP_PORT"],
-    address: ENV["MAILGUN_SMTP_SERVER"],
-    user_name: ENV["MAILGUN_SMTP_LOGIN"],
-    password: ENV["MAILGUN_SMTP_PASSWORD"],
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
     domain: ENV["HOST"],
-    authentication: :plain
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
   ActionMailer::Base.delivery_method = :smtp
 end
