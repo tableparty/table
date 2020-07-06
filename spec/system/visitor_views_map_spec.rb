2567e6cb4956eb74759abf7b5b1bde48cb8ec412
John Bannister <jdbann@gmail.com>
2020-07-06 14:03:43 +0100

    Allow players to move and zoom the map
    Adds the move and zoom actions to the current map template for players.
    These actions do trigger actions on the channel but as the players are
    not the DM the updates do not get saved.

--- spec/system/visitor_views_map_spec.rb
+++ spec/system/visitor_views_map_spec.rb
@@ -1,25 +1,31 @@
 require "rails_helper"
 
 RSpec.describe "Visitor viewing a map", type: :system do
-  it "cannot zoom the map" do
+  it "can zoom the map" do
     map = create :map, name: "Dwarven Excavation"
     map.campaign.update(current_map: map)
 
     visit campaign_path(map.campaign)
 
-    expect(page).not_to have_button("-")
-    expect(page).not_to have_button("+")
+    expect(page).to have_map_with_data(map, "width", "100")
+    expect(page).to have_map_with_data(map, "height", "100")
+    expect(page).to have_button("-", disabled: true)
+
+    find(".current-map__zoom-in").click
+
+    expect(page).to have_map_with_data(map, "width", "200")
+    expect(page).to have_map_with_data(map, "height", "200")
   end
 
-  it "cannot move the map" do
+  it "can move the map" do
     map = create :map, name: "Dwarven Excavation"
     map.campaign.update(current_map: map)
 
     visit campaign_path(map.campaign)
     click_and_move_map(map, from: { x: 300, y: 300 }, to: { x: 50, y: 50 })
 
-    expect(page).to have_map_with_data(map, "x", "50")
-    expect(page).to have_map_with_data(map, "y", "50")
+    expect(page).to have_map_with_data(map, "x", "550")
+    expect(page).to have_map_with_data(map, "y", "550")
   end
 
   it "does not see option to create new maps" do
