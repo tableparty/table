3821689b5c81ef1bdf1736835e717d4d89c20992
John Bannister <jdbann@gmail.com>
2020-07-07 16:27:52 +0100

    Allow enabling fog with environment variable

--- app/features/fog_feature.rb
+++ app/features/fog_feature.rb
@@ -1,7 +1,8 @@
 class FogFeature
   class << self
     def enabled_for?(user)
-      user.email == "jdbann@icloud.com"
+      user.email == "jdbann@icloud.com" ||
+        ENV.fetch("FEATURE_FOG_ENABLED", false) == "true"
     end
   end
 end
--- spec/features/fog_feature_spec.rb
+++ spec/features/fog_feature_spec.rb
@@ -7,9 +7,20 @@ RSpec.describe FogFeature do
       expect(described_class.enabled_for?(user)).to eq true
     end
 
-    it "is not enabled for others" do
+    it "is not enabled for others if FEATURE_FOG_ENABLED is not true" do
+      original_value = ENV["FEATURE_FOG_ENABLED"]
+      ENV["FEATURE_FOG_ENABLED"] = nil
       user = build :user
       expect(described_class.enabled_for?(user)).to eq false
+      ENV["FEATURE_FOG_ENABLED"] = original_value
+    end
+
+    it "is enabled for others if FEATURE_FOG_ENABLED is true" do
+      original_value = ENV["FEATURE_FOG_ENABLED"]
+      ENV["FEATURE_FOG_ENABLED"] = "true"
+      user = build :user
+      expect(described_class.enabled_for?(user)).to eq true
+      ENV["FEATURE_FOG_ENABLED"] = original_value
     end
   end
 end
