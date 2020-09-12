0659f0bd0eb3dc3bb5f7facd2355234b1918f4a1
Chad Pytel <chad@thoughtbot.com>
2020-09-12 08:51:29 -0400

    Move multiple tokens
    Closes #84
    
    Allows you to select tokens and move them together, both around the map and to and from the drawer.
    
    Preserves the old behavior of not needing to select an individual token to move it.

--- spec/system/move_tokens_spec.rb
+++ spec/system/move_tokens_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "move tokens", type: :system do
     expect(page).to have_token_with_data(token, "token-id", token.id)
   end
 
-  it "drags tokens" do
+  it "drags token" do
     map = create :map, :current, zoom: 2
     token = create :token, map: map, x: 0, y: 0, stashed: false
 
@@ -21,6 +21,23 @@ RSpec.describe "move tokens", type: :system do
 
     expect(page).to have_token_with_data(token, "x", 50)
     expect(page).to have_token_with_data(token, "y", 50)
+  end
+
+  it "drags multiple selected tokens" do
+    map = create :map, :current, zoom: 2
+    token_one = create :token, map: map, x: 0, y: 0, stashed: false
+    token_two = create :token, map: map, x: 100, y: 100, stashed: false
+
+    visit campaign_path(map.campaign)
+    wait_for_connection
+    shift_click token_element(token_one)
+    shift_click token_element(token_two)
+    click_and_move_token(token_one, by: { x: 50, y: 50 })
+
+    expect(page).to have_token_with_data(token_one, "x", 50)
+    expect(page).to have_token_with_data(token_one, "y", 50)
+    expect(page).to have_token_with_data(token_two, "x", 150)
+    expect(page).to have_token_with_data(token_two, "y", 150)
   end
 
   it "factors in map zoom when dragging" do
@@ -51,6 +68,30 @@ RSpec.describe "move tokens", type: :system do
     using_session "other user" do
       expect(page).to have_token_with_data(token, "x", 50)
       expect(page).to have_token_with_data(token, "y", 50)
+    end
+  end
+
+  it "moves multiple tokens for other users" do
+    map = create :map, :current, zoom: 2
+    token_one = create :token, map: map, x: 0, y: 0, stashed: false
+    token_two = create :token, map: map, x: 100, y: 100, stashed: false
+
+    visit campaign_path(map.campaign)
+    wait_for_connection
+    using_session "other user" do
+      visit campaign_path(map.campaign)
+      wait_for_connection
+    end
+
+    shift_click token_element(token_one)
+    shift_click token_element(token_two)
+    click_and_move_token(token_one, by: { x: 50, y: 50 })
+
+    using_session "other user" do
+      expect(page).to have_token_with_data(token_one, "x", 50)
+      expect(page).to have_token_with_data(token_one, "y", 50)
+      expect(page).to have_token_with_data(token_two, "x", 150)
+      expect(page).to have_token_with_data(token_two, "y", 150)
     end
   end
 end
--- spec/system/token_drawer_spec.rb
+++ spec/system/token_drawer_spec.rb
@@ -40,10 +40,26 @@ RSpec.describe "token drawer", type: :system do
     wait_for_connection
     token_element = token_element(token)
     token_element.drag_to(token_drawer, html5: true)
-    open_token_drawer
 
     expect(token_drawer).to have_token(token)
     expect(map_element(map)).not_to have_token(token)
+  end
+
+  it "moves multiple tokens from the map to the drawer" do
+    map = create :map, :current
+    token_one = create :token, map: map, stashed: false
+    token_two = create :token, map: map, stashed: false
+
+    visit campaign_path(map.campaign, as: map.campaign.user)
+    wait_for_connection
+    shift_click token_element(token_one)
+    shift_click token_element(token_two)
+    token_element(token_one).drag_to(token_drawer, html5: true)
+
+    expect(token_drawer).to have_token(token_one)
+    expect(map_element(map)).not_to have_token(token_one)
+    expect(token_drawer).to have_token(token_two)
+    expect(map_element(map)).not_to have_token(token_two)
   end
 
   it "hides the token for other users" do
@@ -62,6 +78,28 @@ RSpec.describe "token drawer", type: :system do
 
     using_session "other user" do
       expect(page).not_to have_token(token)
+    end
+  end
+
+  it "hides multiple tokens for other users" do
+    map = create :map, :current
+    token_one = create :token, map: map, stashed: false
+    token_two = create :token, map: map, stashed: false
+
+    visit campaign_path(map.campaign, as: map.campaign.user)
+    wait_for_connection
+    using_session "other user" do
+      visit campaign_path(map.campaign)
+      wait_for_connection
+    end
+
+    shift_click token_element(token_one)
+    shift_click token_element(token_two)
+    token_element(token_one).drag_to(token_drawer, html5: true)
+
+    using_session "other user" do
+      expect(page).not_to have_token(token_one)
+      expect(page).not_to have_token(token_two)
     end
   end
 end
